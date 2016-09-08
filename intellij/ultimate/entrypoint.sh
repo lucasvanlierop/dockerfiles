2924a211ec56ebdf77b9566c137a439e308d0413
Lucas van Lierop <lucas@vanlierop.org>
2016-09-08 12:07:47 +0000

    Added IntelliJ ultimate

---
+++ intellij/ultimate/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+function dump-logo() {
+    base64 /usr/share/git-cola/icons/git.svg
+    exit 0
+}
+
+function start() {
+    # Create a basic user with the same id as the user running starting the container
+    adduser \
+        --no-create-home \
+        --disabled-password \
+        --gecos '' \
+        --uid $LOCAL_UID \
+        $LOCAL_USER > /dev/null
+
+    # Run as the user created above
+    su - $LOCAL_USER -c "intellij"
+}
+
+"$@"
