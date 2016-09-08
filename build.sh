c6ec24891b74ccfb2b6fb6afb8a7924d6dc1ee6f
Lucas van Lierop <lucas@vanlierop.org>
2016-09-08 21:02:07 +0000

    Added support for Rebuilding on entrypoint too

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+DIR=$(readlink -f $(dirname $0))
+
 if [ "$#" == 0 ]; then
   cat <<EOT
 Usage       : [changed|one <name>] [--push]
@@ -30,19 +32,18 @@ validate_diff() {
 }
 
 function build () {
-    local f="$1"
-    local image=${f%Dockerfile}
-    local base=${image%%\/*}
-    local build_dir=$(dirname $(readlink -f $f))
-    local dir_name=$(basename ${build_dir})
-    local docker_file=`readlink -f $f`
-    local version=`grep -P 'ENV VERSION[^\n]*' $docker_file | sed -e 's/ENV VERSION //' || return "0"`
+    local path="$1"
+    local name=${path%%\/*}
+    local context=$(readlink -f $application)
+    local context_name=$(basename ${context})
+    local dockerfile=`readlink -f $application/Dockerfile`
+    local version=`grep -P 'ENV VERSION[^\n]*' $dockerfile | sed -e 's/ENV VERSION //' || return "0"`
     local variant=
     local tag=
-
+    local image_with_tag
     # Set variant if there is one (e.g. IntelliJ Ultimate is a variant of IntelliJ)
-    if [[ "$dir_name" != "$base" ]]; then
-        variant="${dir_name}"
+    if [[ "$context_name" != "$name" ]]; then
+        variant="${context_name}"
     fi
 
     # Use latest as version if there's not a specific version
@@ -57,21 +58,21 @@ function build () {
        tag="${version}"
     fi
 
-    local image_full="$REPO_PREFIX/${base}:${tag}"
-    docker build -t ${image_full} ${build_dir}
+    image_with_tag="$REPO_PREFIX/${name}:${tag}"
+    docker build -t ${image_with_tag} ${context}
 
-    assert_container_dumps_logo $image_full
+    assert_container_dumps_logo $image_with_tag
 
     if [[ $PUSH == true ]]; then
-        docker push ${image_full}
+        docker push ${image_with_tag}
     fi
 
-    echo "Successfully built ${base}:${tag} with context ${build_dir}"
+    echo "Successfully built ${image_with_tag} with context ${context}"
 }
 
 function assert_container_dumps_logo() {
-    local image_full=$1
-    docker run --rm ${image_full} dump-logo | base64 -i --decode > /tmp/logo
+    local image_with_tag=$1
+    docker run --rm ${image_with_tag} dump-logo | base64 -i --decode > /tmp/logo
     local mime_type=$(grep "$(mimetype -b /tmp/logo)" /etc/mime.types | awk '{print $1}')
     if [[ -z $(echo "${mime_type}" | grep "image/") ]]; then
         echo "Logo is not valid, mimetype was ${mime_type}"
@@ -80,14 +81,13 @@ function assert_container_dumps_logo() {
 }
 
 function build_multiple() {
-    local files=$1
-
-    for f in "${files[@]}"; do
-        if ! [[ -e "$f" ]]; then
+    local applications=$1
+    for application in "${applications[@]}"; do
+        if ! [[ -e "$application" ]]; then
             continue
         fi
 
-        build $f
+        build "$application"
     done
 }
 
@@ -104,19 +104,39 @@ function changed() {
 
     VALIDATE_COMMIT_DIFF="$VALIDATE_UPSTREAM...$VALIDATE_HEAD"
 
+    build_multiple "$(get_changed_applications)"
+}
 
-    # get the dockerfiles changed
-    IFS=$'\n'
-    files=( $(validate_diff --name-only -- '*Dockerfile') )
+function get_changed_applications() {
+    declare -A local_changed_dirs
+    IFS=' ' read -r -a changed_files_array <<< "$(echo filter_changed_files $(get_changed_files))"
     unset IFS
 
-    build_multiple $files
+    for f in "${changed_files_array[@]}"; do
+        if ! [[ -e "$f" ]]; then
+            continue
+        fi
+
+        context_name=$(dirname "$f")
+        key="$(echo $context_name | sed -e 's#/#-#')"
+        local_changed_dirs["key"]="$context_name"
+    done
+
+    echo ${local_changed_dirs[@]}
+}
+
+function filter_changed_files() {
+    local $changed_files=$1
+    echo changed_files_we_need="$(echo "$changed_files" | grep -P 'Dockerfile|entrypoint.sh')"
+
+}
+function get_changed_files() {
+    echo $(validate_diff --name-only)
 }
 
 function one() {
     local f=$1
-    build $f/Dockerfile
+    build "$f"
 }
 
 "$@"
-
